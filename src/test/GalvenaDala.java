d31e16a92c7f08fa7d8a169d1eb1f00ba8153519
ElinaKraine <elinaliepkr@gmail.com>
2023-05-27 21:05:58 +0300

    Izveidotas 2 klases un mēģina panākt, lai tas darbojas

--- src/test/GalvenaDala.java
+++ src/test/GalvenaDala.java
@@ -1,10 +1,28 @@
 package test;
 
+import java.awt.event.*;
+import javax.swing.*;
+
 public class GalvenaDala {
-
-	public static void main(String[] args) {
-		
-
-	}
-
-}
+	
+	public static void main(String[] args) {  
+		JFrame f = new JFrame("Kraine_sazKon");
+		//final JTextField tf = new JTextField();
+		//tf.setBounds(50,50, 150,20);
+		JButton b = new JButton("Uzsakt testu");
+		b.setBounds(1,1,120,35);
+		b.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				f.setVisible(false);
+				JOptionPane.showMessageDialog(null, "ss");
+				f.setVisible(true);
+				//tf.setText("Welcome to Javatpoint.");
+			}
+		});
+		f.add(b);
+		//f.add(tf);
+		f.setSize(400,200);
+		f.setLayout(null);
+		f.setVisible(true);
+	}  
+}  
